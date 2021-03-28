83cae9f0cbed2e9f60fe7261146bd79123e7ce91
Kanu <frenchline707@gmail.com>
2021-03-28 22:05:14 +0900

    Refactoring - Json 요청 본문 내 Enum 값 대소문자 구분없이 파싱되도록 처리
    JSON parse : Deserialize value of type `SearchKeywordType` from `String`
    
    요청 바디가 JSON 타입일 경우,
    
    기존: 클라이언트에서 Enum 값에 대해 대소문자 구분하지 않을 경우, 역직렬화 과정에서 파싱에러가 발생
    
    변경: 클라이언트에서 대소문자 신경쓰지 않고 사용할 수 있도록, 서버 측에서 대문자로 처리되도록 수정
    
    - 요청 바디가 JSON 타입일 경우, 컨버터가 먹히지 않으므로 @JsonCreator 를 사용해서 컨버전되도록 처리할 수 있었음

--- src/main/java/com/tena/untact2021/dto/Search.java
+++ src/main/java/com/tena/untact2021/dto/Search.java
@@ -1,5 +1,6 @@
 package com.tena.untact2021.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
@@ -45,12 +46,25 @@ public class Search {
 
         public static boolean isDefined(String searchKeywordType) {
             return Arrays
-                    .stream(values())
+                    .stream(SearchKeywordType.values())
                     .anyMatch(s -> s.getKey().equals(searchKeywordType.toUpperCase()));
         }
 
         public static boolean isNotDefined(String searchKeywordType) {
             return !isDefined(searchKeywordType);
+        }
+
+        /**
+         ** <p>Convert Before Biding Parameter (using Setter) annotated with @RequestBody</p>
+         ** <p>- 클라이언트 쪽에서 대소문자 신경쓰지 않고 사용하도록 서버 측에서 처리함</p>
+         ** <p>- Request Body가 JSON 타입이면 Converter는 무용지물이므로, @JsonCreator 사용해 파싱</p>
+         */
+        @JsonCreator
+        public static SearchKeywordType from(String source) {
+            log.debug("SearchKeywordType.from");
+            log.debug("source: {}", source);
+
+            return SearchKeywordType.valueOf(source.toUpperCase());
         }
     }
 
