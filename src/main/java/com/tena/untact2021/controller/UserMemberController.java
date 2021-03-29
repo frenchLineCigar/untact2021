202bbf197856e14eb0679dac566b5a58fddc112c
Lucy <begantofly@naver.com>
2021-03-29 20:27:23 +0900

    로그인, 로그아웃

--- src/main/java/com/tena/untact2021/controller/UserMemberController.java
+++ src/main/java/com/tena/untact2021/controller/UserMemberController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.security.acl.Group;
 import java.util.Map;
 
@@ -22,13 +24,17 @@ public class UserMemberController {
     @RequestMapping("/user/member/doJoin")
     @ResponseBody
     public ResultData doJoin(@RequestParam Map<String, Object> param) {
+
+        // TODO : 리팩토링
+        //  HandlerMethodArgumentResolver + JSR-303 사용(DTO) 및 Validator 구현(@InitBinder)
+
         if (param.get("loginId") == null) {
             return new ResultData("F-1", "loginId를 입력해주세요.");
         }
 
-        Member exsistingMember = memberService.getMemberByLoginId((String) param.get("loginId"));
+        Member existingMember = memberService.getMemberByLoginId((String) param.get("loginId"));
 
-        if (exsistingMember != null) {
+        if (existingMember != null) {
             return new ResultData("F-2", String.format("%s (은)는 이미 사용 중인 로그인 아이디 입니다.", param.get("loginId")));
         }
 
@@ -53,6 +59,56 @@ public class UserMemberController {
         }
 
         return memberService.joinMember(param);
+    }
+
+    /* 회원 로그인 */
+    @RequestMapping("/user/member/doLogin")
+    @ResponseBody
+    public ResultData doLogin(String loginId, String loginPw, HttpSession session) {
+
+        if (session.getAttribute("loggedInMemberId") != null) {
+            return new ResultData("F-4", "이미 로그인 되었습니다.");
+        }
+
+        // TODO : 리팩토링
+        //  HandlerMethodArgumentResolver + JSR-303 사용(DTO) 및 Validator 구현(@InitBinder)
+
+        if (loginId == null) {
+            return new ResultData("F-1", "loginId를 입력해주세요.");
+        }
+
+        Member existingMember = memberService.getMemberByLoginId((String) loginId);
+
+        if (existingMember == null) {
+            return new ResultData("F-2", "존재하지 않는 로그인 아이디 입니다.", "loginId", loginId);
+        }
+
+        if (loginPw == null) {
+            return new ResultData("F-1", "비밀번호를 입력해주세요.");
+        }
+
+        if (existingMember.getLoginPw().equals(loginPw) == false) {
+            return new ResultData("F-3", "비밀번호가 일치하지 않습니다.");
+        }
+
+        session.setAttribute("loggedInMemberId", existingMember.getId());
+
+        return new ResultData("S-1", String.format("%s님 환영합니다.", existingMember.getNickname()));
+    }
+
+    /* 회원 로그아웃 */
+    @RequestMapping("/user/member/doLogout")
+    @ResponseBody
+    public ResultData doLogout(HttpSession session) {
+
+        if (session.getAttribute("loggedInMemberId") == null) {
+            return new ResultData("S-2", "이미 로그아웃 되었습니다.");
+        }
+
+        //session.removeAttribute("loggedInMemberId");
+        session.invalidate();
+
+        return new ResultData("S-1", "로그아웃 되었습니다.");
     }
 
 }
